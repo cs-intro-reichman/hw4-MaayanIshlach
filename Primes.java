67537b171908d21aee5d90af6b0c1faab24db490
Maayan Ishlach <maayanishlach@gmail.com>
2024-12-05 09:51:30 +0200

    try

--- Primes.java
+++ Primes.java
@@ -3,15 +3,17 @@ public class Primes {
         int n=Integer.parseInt(args[0]);
         int count=0;
         double per;
+        System.out.println("Prime numbers up to "+n+":");
         for (int i=0;i<n;i++)
         {
             if (Is_prime(i)) {
+                System.out.println(i);
                 count++;
             }
         }
         per=((double)count/(double)n)*100.0 ;
         System.out.println("There are "+ count + " primes between 2 and "+ n+ " (" + (int)per+ "% are primes)" );
-        //There are 10 primes between 2 and 30 (33% are primes)
+        //Prime numbers up to 7:%0A2%0A3%0A5%0A7%0AThere are 4 primes between 2 and 7 (57%25 are primes)
         }
     
 
